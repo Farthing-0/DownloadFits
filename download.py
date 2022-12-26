e5a8a3d937d4ddfd0716f281097d437564e7a93d
Quanquan Hu <huquanquan22@mails.ucas.ac.cn>
2022-12-26 22:52:03 +0800

    Enhance the stability of multi-threaded downloads

--- download.py
+++ download.py
@@ -18,63 +18,51 @@ def read_file_list(file):
             file_list.append(file)
     return file_list
 
-def download_multi_task(file_list,url_prefix,task_index,task_num,start_time):
-    lc_full_size = 0
-    dvt_full_size = 0
-    full_file_counter = 0
-    for index,file in enumerate(file_list):
-        if(index % task_num == task_index):
-            if os.path.isfile(file):
-                if(file.endswith('s_lc.fits')):
-                    size = os.path.getsize(dir+file)
-                    if lc_full_size == 0:
-                        print('Evaluate full size of lc file By' + file)
-                        wget.download(url_prefix + file, out=file)
-                        lc_full_size = os.path.getsize(dir+file)
-                        print('Full size of lc file is ' + str(lc_full_size))
-                        continue
-                    if size < lc_full_size:
-                        full_file_counter = 0
-                        os.remove(dir+file)
-                        print('File is not complete, downloading ' + file)
-                        wget.download(url_prefix + file, out=file)                    
-                    else:
-                        full_file_counter += 1
-                        print('Skip ' + str(full_file_counter) + ' files',end='\r',flush=True)
-                elif(file.endswith('dvt.fits')):
-                    size = os.path.getsize(dir+file)
-                    remote_size = int(requests.head(url_prefix + file).headers['content-length'])
-                    if size < remote_size:
-                        full_file_counter = 0
-                        os.remove(dir+file)
-                        print('File is not complete, downloading ' + file)
-                        wget.download(url_prefix + file, out=file)
-                    else:
-                        full_file_counter += 1
-                        print('Skip ' + str(full_file_counter) + ' files',end='\r',flush=True)
-            else:
-                print('Downloading ' + file)
-                wget.download(url_prefix + file, out=file)
-            print('Download '+str(index)+'/'+str(len(file_list))+' complete.')
-            print('assumed time left: ' + str((time.time() - start_time) / (index + 1) * (len(file_list) - index - 1) ))
-    print('Download complete.')
-
-def download_multi(file_list):
-    from multiprocessing import  Process
-    start_time = time.time()
-    process_num = 30
-    process_list = []
+def download_file(file_name,file_type,file_size = 0):
     url_prefix = 'https://mast.stsci.edu/api/v0.1/Download/file/?uri=mast:TESS/product/'
-    for i in range(process_num):
-        p = Process(target=download_multi_task,args=(file_list,url_prefix,i,process_num,start_time))
-        p.start()
-        process_list.append(p)
-    for i in process_list:
-        p.join()
+    # wget.download(url_prefix + file_name, out=file_name)
+    if(file_type == 'lc'):
+        if(os.path.isfile(file_name)):
+            size = os.path.getsize(file_name)
+            if size < file_size:
+                os.remove(file_name)
+                print('File is not complete, downloading ' + file_name)
+                wget.download(url_prefix + file_name, out=file_name)
+            else:
+                print('Skip ' + file_name)
+        else:
+            print('Downloading ' + file_name)
+            wget.download(url_prefix + file_name, out=file_name)
+    elif(file_type == 'dvt'):
+        if(os.path.isfile(file_name)):
+            size = os.path.getsize(file_name)
+            remote_size = int(requests.head(url_prefix + file_name).headers['content-length'])
+            if size < remote_size:
+                os.remove(file_name)
+                print('File is not complete, downloading ' + file_name)
+                wget.download(url_prefix + file_name, out=file_name)
+            else:
+                print('Skip ' + file_name)
+        else:
+            print('Downloading ' + file_name)
+            wget.download(url_prefix + file_name, out=file_name)
+
+def download_multi_files(file_list,file_type,file_size = 0):
+    import concurrent.futures
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        futures = [executor.submit(download_file,file_name,file_type,file_size) for file_name in file_list]
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
 
 if __name__ == "__main__":
     dir = './'
+    url_prefix = 'https://mast.stsci.edu/api/v0.1/Download/file/?uri=mast:TESS/product/'
     file_list = []
     with open(find_sh_files(dir), 'r') as f:
         file_list = read_file_list(f)
-    download_multi(file_list)
+    if(file_list[0].endswith('s_lc.fits')):
+        print('Searching lc file size...')
+        remote_size = int(requests.head(url_prefix + file_list[0]).headers['content-length'])
+        download_multi_files(file_list,'lc',remote_size)
+    elif(file_list[0].endswith('dvt.fits')):
+        download_multi_files(file_list,'dvt')
